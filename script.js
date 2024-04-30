65d7c55215a1c4e59088afd55bdfd5c9861e15f0
Mathews1501 <matthewchipeta@outlook.com>
2024-04-30 07:32:28 +0200

    index

--- script.js
+++ script.js
@@ -24,4 +24,24 @@ document.addEventListener("DOMContentLoaded", function () {
       resetAndRestartAnimation(bar);
     });
   });
+
+  //js for my scroll up button
+document.addEventListener("DOMContentLoaded", function() {
+  var scrollUpButton = document.getElementById('scrollUpBtn');
+
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > 300) {
+      scrollUpButton.style.display = 'block';
+    } else {
+      scrollUpButton.style.display = 'none';
+    }
+  });
+
+  scrollUpButton.addEventListener('click', function() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+});
   

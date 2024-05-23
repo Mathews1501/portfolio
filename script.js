318f4165963700148d7432d14434730f089a7844
Mathews1501 <matthewchipeta@outlook.com>
2024-05-23 15:22:40 +0200

    updated ui

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Get all the skill bars
-    const bars = document.querySelectorAll(".bar");
-  
-    // Define a function to handle animation reset and restart
-    function resetAndRestartAnimation(bar) {
-      // Reset the bar width to 0 and restart animation
-      bar.style.width = "100%";
-      bar.style.animation = "none";
-      void bar.offsetWidth; // Trigger reflow to restart animation
-      bar.style.animation = "progress-animation 3s ease forwards";
-    }
-  
-    // Iterate over each skill bar
-    bars.forEach(bar => {
-      // Listen for animation end on each bar
-      bar.addEventListener("animationend", () => {
-        resetAndRestartAnimation(bar);
-      });
-    });
-  
-    // Call the resetAndRestartAnimation function for each bar to start the animation initially
-    bars.forEach(bar => {
-      resetAndRestartAnimation(bar);
-    });
-  });
-
   //js for my scroll up button
 document.addEventListener("DOMContentLoaded", function() {
   var scrollUpButton = document.getElementById('scrollUpBtn');
@@ -70,9 +43,6 @@ document.querySelector('.read-more').addEventListener('click',function(){
   const content = document.querySelector('.content');
   const aboutParagraph2 = document.querySelector('.about-paragraph2');
   const readMore = document.querySelector('.read-more');
-
-  
-
 
 });
   

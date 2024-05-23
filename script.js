8b5cd966726ccf0de1aa6b19b8dd8459692c6fc7
Mathews1501 <matthewchipeta@outlook.com>
2024-05-23 15:55:25 +0200

    more changes

--- script.js
+++ script.js
@@ -18,31 +18,3 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-  const headings = document.querySelectorAll('.heading');
-
-  setInterval(() => {
-    let activeHeading = document.querySelector('.heading.active');
-    let nextHeading = activeHeading.nextElementSibling;
-    
-    if (!nextHeading) {
-      nextHeading = headings[0];
-    }
-
-    activeHeading.classList.remove('active');
-    nextHeading.classList.add('active');
-    activeHeading.classList.add('next');
-
-    setTimeout(() => {
-      activeHeading.classList.remove('next');
-    }, 1000); // transition duration
-  }, 5000); // Change heading every 5 seconds (adjust as needed)
-});
-
-document.querySelector('.read-more').addEventListener('click',function(){
-  const content = document.querySelector('.content');
-  const aboutParagraph2 = document.querySelector('.about-paragraph2');
-  const readMore = document.querySelector('.read-more');
-
-});
-  

77234253eb2e1eb6aeb7d00f3cefbb2cb6d24196
Mathews1501 <matthewchipeta@outlook.com>
2024-06-07 11:45:42 +0200

    html, css, Js updated

--- script.js
+++ script.js
@@ -18,3 +18,14 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
+// sidebar menu js
+
+function showSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'flex'
+}
+
+function hideSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none'
+}
